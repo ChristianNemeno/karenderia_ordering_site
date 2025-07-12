f194e5b8423d312fbc05cfd80dda333aaba655fb
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-07-12 22:48:00 +0800

    fix gg

--- kan-anan-app/vite.config.js
+++ kan-anan-app/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/karenderia_ordering_site/",
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/karenderia_ordering_site/' : '/',
   plugins: [react()],
-})
+}));
